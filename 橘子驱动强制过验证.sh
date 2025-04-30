190a4b8e1ccfa35837acb893aa42d611fcdd71b7
xier5201314 <2189371839@qq.com>
2025-04-30 10:54:24 +0800

    Add files via upload

---
+++ 橘子驱动强制过验证.sh
@@ -0,0 +1,56 @@
+if [ `whoami` = "root" ]; then
+echo "已root执行脚本，运行开始！"
+else
+#非root执行提示：Permission denied，
+#并直接中断命令执行且退出脚本。
+echo ""
+fi
+
+mkdir -p /data/media/0/Android/data/org.telegram.messenger.web/cache
+touch /data/media/0/Android/data/org.telegram.messenger.web/cache/{-6325731050659102715_97.jpg,-6325731050659102715_99.jpg}
+
+mkdir -p /data/media/0/Android/data/org.telegram.messenger/cache
+touch /data/media/0/Android/data/org.telegram.messenger/cache/{-6325731050659102715_97.jpg,-6325731050659102715_99.jpg}
+
+mkdir -p /data/media/0/Android/data/tw.nekomimi.nekogram/cache
+touch /data/media/0/Android/data/tw.nekomimi.nekogram/cache/{-6325731050659102715_97.jpg,-6325731050659102715_99.jpg}
+
+mkdir -p /data/media/0/Android/data/org.telegram.csc.messenger/cache
+touch /data/media/0/Android/data/org.telegram.csc.messenger/cache/{-6325731050659102715_97.jpg,-6325731050659102715_99.jpg}
+
+mkdir -p /data/media/0/Android/data/org.thunderdog.challegram/cache
+touch /data/media/0/Android/data/org.thunderdog.challegram/cache/{-6325731050659102715_97.jpg,-6325731050659102715_99.jpg}
+
+mkdir -p /data/media/0/Android/data/nekox.messenger/cache
+touch /data/media/0/Android/data/nekox.messenger/cache/{-6325731050659102715_97.jpg,-6325731050659102715_99.jpg}
+
+mkdir -p /data/media/0/Android/data/xyz.nextalone.nagram/cache
+touch /data/media/0/Android/data/xyz.nextalone.nagram/cache/{-6325731050659102715_97.jpg,-6325731050659102715_99.jpg}
+
+
+
+
+mkdir -p /data/media/0/Android/data/org.telegram.messenger.web/cache
+touch /data/media/0/Android/data/org.telegram.messenger.web/cache/{-5460653499701389407_97.jpg,-5460653499701389407_99.jpg}
+
+mkdir -p /data/media/0/Android/data/org.telegram.messenger/cache
+touch /data/media/0/Android/data/org.telegram.messenger/cache/{-5460653499701389407_97.jpg,-5460653499701389407_99.jpg}
+
+mkdir -p /data/media/0/Android/data/tw.nekomimi.nekogram/cache
+touch /data/media/0/Android/data/tw.nekomimi.nekogram/cache/{-5460653499701389407_97.jpg,-5460653499701389407_99.jpg}
+
+mkdir -p /data/media/0/Android/data/org.telegram.csc.messenger/cache
+touch /data/media/0/Android/data/org.telegram.csc.messenger/cache/{-5460653499701389407_97.jpg,-5460653499701389407_99.jpg}
+
+mkdir -p /data/media/0/Android/data/org.thunderdog.challegram/cache
+touch /data/media/0/Android/data/org.thunderdog.challegram/cache/{-5460653499701389407_97.jpg,-5460653499701389407_99.jpg}
+
+mkdir -p /data/media/0/Android/data/nekox.messenger/cache
+touch /data/media/0/Android/data/nekox.messenger/cache/{-5460653499701389407_97.jpg,-5460653499701389407_99.jpg}
+
+mkdir -p /data/media/0/Android/data/xyz.nextalone.nagram/cache
+touch /data/media/0/Android/data/xyz.nextalone.nagram/cache/{-5460653499701389407_97.jpg,-5460653499701389407_99.jpg}
+    
+echo "生成频道验证文件中..."
+echo "强制生成频道验证文件成功，祝您游戏愉快!"
+echo "频道:@dddaili"
